ab1c151512516515bfc6a14e5558da2e474f449b
Rafael Ramos <ramos.aqueda@gmail.com>
2022-06-13 23:24:53 -0400

    entrega final

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 // middlewares: validations
-//const { authCheck, adminCheck } = require("../middlewares/auth");
+const { authCheck, adminCheck } = require('../middlewares/auth');
 
 // controller
 const { createOrUpdateUser, currentUser } = require('../controllers/auth');
 
 // endpoints routes
-router.post('/create-or-update-user', createOrUpdateUser);
-router.post('/current-user', currentUser);
-router.post('/current-admin', currentUser);
+router.post('/create-or-update-user', authCheck, createOrUpdateUser);
+router.post('/current-user', authCheck, currentUser);
+router.post('/current-admin', authCheck, adminCheck, currentUser);
 
 module.exports = router;
